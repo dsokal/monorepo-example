6fe7f9904e2d65ad39cd72f594f35fa39df8b4d8
Dominik Sokal <dominik.sokal@swmansion.com>
2021-01-18 12:17:21 +0100

    use lib from another workspace

--- rnapp/App.js
+++ rnapp/App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { sum } from 'somelib';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>1 + 2 = {sum(1,2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
